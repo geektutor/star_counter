74d9f3080208dc892672cde02425d322051305b6
geektutor <sodiq.akinjobi@gmail.com>
2020-07-12 13:13:10 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,97 +3,86 @@ import 'package:flutter/material.dart';
 import 'star_counter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+bool darkMode = false;
+
 void main() {
   runApp(StarCounterApp());
 }
 
-class StarCounterApp extends StatelessWidget {
+class StarCounterApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      routes: {
-        '/': (context) => HomePage(),
-      },
-    );
-  }
+  _StarCounterAppState createState() => _StarCounterAppState();
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+class _StarCounterAppState extends State<StarCounterApp> {
   String _repositoryName = "GitHub";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 400),
-          child: Card(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'GitHub Star Counter',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Enter a GitHub repository',
-                      hintText: 'geektutor/wewe',
-                    ),
-                    onSubmitted: (text) {
+    return MaterialApp(
+      theme: darkMode ? ThemeData.dark() : ThemeData.light(),
+      routes: {
+        '/': (context) => Scaffold(
+              appBar: AppBar(
+                title: Text('GitHub Star Counter'),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.brightness_3),
+                    onPressed: () {
                       setState(() {
-                        _repositoryName = text;
+                        darkMode = !darkMode;
                       });
                     },
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 32.0),
-                    child: GitHubStarCounter(
-                      repositoryName: _repositoryName, // New
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FlatButton(
-                        color: Colors.transparent,
-                        textColor: Colors.blue,
-                        onPressed: () => launch(
-                          Active.activeLink
-                              ? 'https://github.com/$_repositoryName'
-                              : 'https://github.com/',
-                          enableJavaScript: true,
-                          enableDomStorage: true,
-                        ),
-                        child: Text('Go to GitHub'),
-                      ),
-                      FlatButton(
-                        color: Colors.transparent,
-                        textColor: Colors.blue,
-                        onPressed: () => launch(
-                          '/#/privacypolicy',
-                          enableJavaScript: true,
-                          enableDomStorage: true,
-                        ),
-                        child: Text('Privacy Policy'),
-                      ),
-                    ],
-                  ),
+                  )
                 ],
               ),
+              body: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: 400),
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Enter a GitHub repository',
+                              hintText: 'geektutor/wewe',
+                            ),
+                            onSubmitted: (text) {
+                              setState(() {
+                                _repositoryName = text;
+                              });
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 32.0),
+                            child: GitHubStarCounter(
+                              repositoryName: _repositoryName, // New
+                            ),
+                          ),
+                          FlatButton(
+                            color: Colors.transparent,
+                            textColor: Colors.blue,
+                            onPressed: () => launch(
+                              Active.activeLink
+                                  ? 'https://github.com/$_repositoryName'
+                                  : 'https://github.com/',
+                              enableJavaScript: true,
+                              enableDomStorage: true,
+                            ),
+                            child: Text('Go to GitHub'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
+      },
     );
   }
 }
